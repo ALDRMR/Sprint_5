88e64a6baf1cc63816142a46ed70065a6b39124f
ALDRMR <amarinov@yandex.ru>
2026-09-20 19:57:54 +0300

    выделить параметризованные тесты в отдельный класс

--- src/test/java/com/example/LionNotParameterizedTests.java
+++ src/test/java/com/example/LionNotParameterizedTests.java
@@ -2,31 +2,18 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class LionTests {
+public class LionNotParameterizedTests {
 
     @Mock
     Feline feline;
-
-    @ParameterizedTest(name = "Для пола {0} doesHaveMane должно быть {1}")
-    @CsvSource({
-            "Самец, true",
-            "Самка, false"
-    })
-    public void lionConstructorTestManePresence(String sex, boolean expectedHasMane) throws Exception {
-        Lion lion = new Lion(sex, feline);
-        assertEquals(expectedHasMane, lion.doesHaveMane(), "Для пола {0} doesHaveMane должно быть {1}");
-    }
 
     @Test
     public void lionConstructorTestInvalidSexThrowsProperExceptionMessage() {
